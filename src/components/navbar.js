d07ba56596ac63c9e5e43668c47bb875e2b1373d
step-github <geral@meetstep.com>
2025-06-10 14:48:03 +0100

    logo

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -136,7 +136,7 @@ export default function Navbar({ router }) {
               padding: 0,
             }}
           >
-            <Image src="/a.png" alt="Logo" width={1080} height={1920} />
+            <Image src="/a.webp" alt="Logo" width={1080} height={1920} />
           </Link>
         )}
         <div
